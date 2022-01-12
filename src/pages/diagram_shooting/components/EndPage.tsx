511fb60209c00cabc4622e87d92fb29878f74d9c
Anastasiya Rychkova <a.k.rychkova@yandex.ru>
2022-01-12 16:43:57 +0300

    Добавляет сраницы завершения эксперимента

---
+++ src/pages/diagram_shooting/components/EndPage.tsx
@@ -0,0 +1,18 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+import ModalWindowBuilder from '../../../components/ModalWindow/ModalWindowBuilder';
+import page from '../ExpPage';
+import progress from '../ExpProcess';
+
+const EndPage = observer( () => {
+	return (
+		<div className="page">
+			<ModalWindowBuilder
+				provider={page}
+				variant={progress.variant}
+				assessments={progress.assessments} />
+		</div>
+	);
+} );
+
+export default EndPage;
