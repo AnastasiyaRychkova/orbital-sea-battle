8a6ca6fc9e376983ddcb91ce1fd34327bf731e98
Anastasia Rychkova <anast.ry4kova2011@yandex.ru>
2023-05-28 23:40:49 +0300

    feat: ship & en rules

--- src/pages/landing-alt/modules/mainPage/WelcomePage/WelcomePage.tsx
+++ src/pages/landing-alt/modules/mainPage/WelcomePage/WelcomePage.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import cn from 'classnames';
 import { useTranslation } from 'react-i18next';
 import { ButtonWithIcon } from 'components/Button/WithIcon/Button';
-import { IconButton } from 'components/Button/IconButton/Button';
 
-import imgPlayVideo from '../../../img/common/play-video.svg';
-import imgShip from '../../../img/mainPage/ship.svg';
 import style from './WelcomePage.module.scss';
-import styleButton from '../../buttons/Button.module.scss';
 
 
 const WelcomePage = () => {
@@ -17,39 +12,22 @@ const WelcomePage = () => {
 		<div className={style['welcome-page']}
 			id="welcome"
 		>
+			<img src="img/landing/ship.svg"
+				className={style.ship}
+				alt=""
+			/>
 			<div className={style.header}>
 				<h1 className="header-1">Orbital Battleship</h1>
 				<span className="header-5 bold">
 					{t( 'educational' )}
 				</span>
 			</div>
-			<div className={style.content}>
-				<img src={imgShip}
-					className={cn( style.ship, style.left )}
-					alt=""
-				/>
-				<img src={imgShip}
-					className={cn( style.ship, style.right )}
-					alt=""
-				/>
-				<div className={style.video}>
-					<button className={styleButton['button-icon']}>
-						<img
-							src={imgPlayVideo}
-							className={cn( style['play-video'] )}
-							alt=""
-						/>
-					</button>
-				</div>
-				<ButtonWithIcon
-
-					glyph="play"
-					value={t( 'play' )}
-					priority="primary"
-				/>
-			</div>
-			<IconButton
-				glyph="scroll"
+			<ButtonWithIcon
+				className={style.play}
+				glyph="play"
+				value={t( 'play' )}
+				priority="primary"
+				to="play"
 			/>
 		</div>
 	);
